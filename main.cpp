2c6d0da712ddcbb0f35dc3c80663acef7ea7c5b6
Ross <ross@andrewarthacornwall.com>
2020-01-30 12:24:12 +0000

    start working on program storage part to control devices

--- main.cpp
+++ main.cpp
@@ -5,23 +5,38 @@
 
 #include "mbed.h"
 
+//Define DigitalOutputs for the utilised devices
 DigitalOut washPump(PE_14);
 DigitalOut samplePump(PE_15);
 DigitalOut switchValve(PE_12);
 DigitalOut solenoidValve(PE_8);
 
+//Define enumerated types for the devices
+enum DEVICE {WASHPUMP, SAMPLEPUMP, SWITCHVALVE, SOLENOIDVALVE}; 
+
+//Define a structure to hold the timing parameters
 typedef struct {
-    int device;
+    enum DEVICE device;
     int timeOn;
     int timeOff;
 } deviceControl;
 
+typedef struct {
+    deviceControl StepTimes[4];
+} step;
+
+step routine[2] = {routine[0].StepTimes[0] = {WASHPUMP, 0, 20}, routine[0].StepTimes[1] = {WASHPUMP, 0, 20}, routine[0].StepTimes[2] = {WASHPUMP, 0, 20}, routine[0].StepTimes[3] = {WASHPUMP, 0, 20}};
+
+
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS  500
 
-
 int main()
 {
+    routine[0].StepTimes[0].device = SAMPLEPUMP;
+
+    enum DEVICE x = routine[0].StepTimes[0].device;// = WASHPUMP;
+	
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
 
