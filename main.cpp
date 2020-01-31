20bdc2ee67fd4fe175267c8b5167767aea5bebfe
Ross <ross@andrewarthacornwall.com>
2020-01-31 15:57:36 +0000

    Work on timing implementation

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@ DigitalOut switchValve(PE_12);
 DigitalOut solenoidValve(PE_8);
 
 
+//Define how many devices there are
+#define NO_DEVICES 4
+
 //Define enumerated types for the devices
 enum DEVICE {WASHPUMP, SAMPLEPUMP, SWITCHVALVE, SOLENOIDVALVE}; 
 
@@ -25,18 +28,107 @@ typedef struct {
 } deviceTime;
 
 typedef struct {
-    deviceTime StepTimes[4];
+    deviceTime StepTimes[NO_DEVICES];
 } step;
 
-
-step routine[2] = {routine[0].StepTimes[0] = {WASHPUMP, 0, 20}, routine[0].StepTimes[1] = {WASHPUMP, 0, 20}, routine[0].StepTimes[2] = {WASHPUMP, 0, 20}, routine[0].StepTimes[3] = {WASHPUMP, 0, 20}};
+#define STEPS 2
+step routine[STEPS] = {routine[0].StepTimes[0] = {WASHPUMP, 0, 20}, routine[0].StepTimes[1] = {WASHPUMP, 0, 20}, routine[0].StepTimes[2] = {WASHPUMP, 0, 20}, routine[0].StepTimes[3] = {WASHPUMP, 0, 20}};
 
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS  500
 
-int main()
-{
+
+//Turn off device
+void turnOff(enum DEVICE device) {
+    switch (device) {
+        case WASHPUMP : {
+            //turn off wash pump
+            break;
+        }
+
+        case SAMPLEPUMP : {
+            //turn off sample pump
+            break;
+        } 
+
+        case SWITCHVALVE : {
+            //turn off switch valve
+            break;
+        }
+
+        case SOLENOIDVALVE : {
+            //turn off solenoid valve
+            break;
+        }
+
+        default: {
+            //Default case, should not happen
+            //Error log - unregcognised device passed to turnOff
+            break;
+        }
+    }
+}
+
+//Turn on device
+void turnOn(enum DEVICE device) {
+    switch (device) {
+        case WASHPUMP : {
+            //turn on wash pump
+            break;
+        }
+
+        case SAMPLEPUMP : {
+            //turn on sample pump
+            break;
+        } 
+
+        case SWITCHVALVE : {
+            //turn on switch valve
+            break;
+        }
+
+        case SOLENOIDVALVE : {
+            //turn on solenoid valve
+            break;
+        }
+
+        default: {
+            //Default case, should not happen
+            //Error log - unregcognised device passed to turnOn
+            break;
+        }
+    }
+}
+
+//Device timings
+void checkTiming(void) {
+
+    //Temp, bind to rtc for accuracy
+    static int timeElapsed = 0;
+
+    //Loop through structures, read the timing data and take action if appropiate
+    for (int i = 0; i < STEPS; i++) {
+        for (int j = 0; j < NO_DEVICES; j++) {
+
+            //Is it time to turn a device on
+            if (timeElapsed == routine[STEPS].StepTimes[NO_DEVICES].timeOn) {
+                //Turn on the device
+                turnOn(routine[STEPS].StepTimes[NO_DEVICES].device)
+            }
+
+            //Is it time to turn a device off
+            else if (timeElapsed == routine[STEPS].StepTimes[NO_DEVICES].timeOff) {
+                //Turn off the device
+                turnOff(routine[STEPS].StepTimes[NO_DEVICES].device);
+            }
+        }
+    }
+    //Increment timeElapsed
+    timeElapsed++;
+}
+
+int main() {
 	
     routine[0].StepTimes[0].device = SAMPLEPUMP;
 	
