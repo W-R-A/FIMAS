106658d6519f6107737c112d9ba6a4749d2cf639
Ross <ross@andrewarthacornwall.com>
2020-02-27 14:58:30 +0000

    Use modern api

--- network/Networkbits.cpp
+++ network/Networkbits.cpp
@@ -9,8 +9,8 @@ void networktest()
     eth.connect();
     
     //Now setup a web server
-    TCPServer srv;           //TCP/IP Server
-    TCPSocket clt_sock;      //Socket for communication
+    TCPSocket srv;           //TCP/IP Server
+    TCPSocket* clt_sock;      //Socket for communication
     SocketAddress clt_addr;  //Address of incoming connection
     
     /* Open the server on ethernet stack */
@@ -28,20 +28,29 @@ void networktest()
         using namespace std;
         
         //Block and wait on an incoming connection
-        srv.accept(&clt_sock, &clt_addr);
+        clt_sock=srv.accept();
+		
         printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
         
         //Uses a C++ string to make it easier to concatinate
-        string response;
-        //This is a C string
-        char ldr_str[] = "test";
+        string response = "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n";
         
         //Build the C++ string response
-        response = HTTP_MESSAGE_BODY1;
-        response += i;
-        response += HTTP_MESSAGE_BODY2;
-        
-        //Send static HTML response (as a C string)
-        clt_sock.send(response.c_str(), response.size()+1);    
+        response =+ HTTP_MESSAGE_BODY1;
+		
+		//Send static HTML response (as a C string)
+        clt_sock->send(response.c_str(), response.size());  
+		
+		response = "HTTP/1.0 200 OK\r\nContent-Type: application/javascript; charset=utf-8\r\n";
+		
+		//Build the C++ string response
+        response = jquery;
+		
+		//Send static HTML response (as a C string)
+        clt_sock->send(response.c_str(), response.size()); 
+
+		clt_sock->close();
+		
+		wait_us(100000);
     }
 }
