ab6c2a91094b5a117433fca6d8c57b3532934116
Ross <ross@andrewarthacornwall.com>
2020-02-28 21:58:25 +0000

    Send headers

--- network/Networkbits.cpp
+++ network/Networkbits.cpp
@@ -26,10 +26,12 @@ void networktest()
     
 	int i = 0;
     //KEEP RESPONDING WHILE THE SWITCHES ARE PRESSED
+	
+	//this will run once per http request
     while (true) {
         using namespace std;
 
-        char *buffer = new char[2048];
+        char *buffer = new char[1024];
         
         //Block and wait on an incoming connection
         clt_sock = srv.accept();
@@ -38,26 +40,63 @@ void networktest()
 
         pc.printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
 
-        clt_sock->recv(buffer, 2048);
+        clt_sock->recv(buffer, 1024);
 
-        pc.printf("Received Msg: %s\n", buffer);  //this was missing in original example. 
+        pc.printf("Received Msg: %s\n\n", buffer);  //this was missing in original example. 
+		
+		//address parser logic, decide what repsonse is required dependant on the incoming address
+		
+		string addFavicon ("favicon");
+		string address (buffer);
+		
+		
+		string response;
+		
+		
+		    //Read All from buffer
+		if (address.find(addFavicon) != string::npos) {
+			//Echo confirmation
+			pc.printf("favicon, 404");
+			
+			//Uses a C++ string to make it easier to concatinate
+			response += HTTP_STATUS_LINE_404;
+			
+			response += "\r\n";
+		
+		} 
+		else {
+			//Uses a C++ string to make it easier to concatinate
+			response += HTTP_STATUS_LINE_200;
+			
+			response += "\r\n";
+			
+			response += HTTP_HEADER_FIELDS;
+			
+			response += "\r\n\r\n";
+			
+			//Build the C++ string response
+			response += HTTP_MESSAGE_BODY1;
+		}
+		
+		
         
-        //Uses a C++ string to make it easier to concatinate
-        string response = "HTTP/1.0 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n";
         
-        //Build the C++ string response
-        response =+ HTTP_MESSAGE_BODY1;
+		
+		//print send html
+		printf("\n\nHTML: %s", response.c_str());
 		
 		//Send static HTML response (as a C string)
-        //clt_sock->send(response.c_str(), response.size());  
+        clt_sock->send(response.c_str(), response.size());  
 		
-		response = "HTTP/1.0 200 OK\r\n Content-Type: application/javascript; charset=utf-8\r\n";
+		
+		
+		//response = "HTTP/1.0 200 OK\r\n Content-Type: application/javascript; charset=utf-8\r\n";
 		
 		//Build the C++ string response
-        response =+ jquery;
+        //response =+ jquery;
 		
 		//Send static HTML response (as a C string)
-        clt_sock->send(response.c_str(), response.size()); 
+        //clt_sock->send(response.c_str(), response.size()); 
 
 		clt_sock->close();
 		
