b17c46968f04948cc7452f47d667a51fae7b1961
Ross <ross@andrewarthacornwall.com>
2020-02-29 16:47:14 +0000

    Update comments

--- network/Networkbits.cpp
+++ network/Networkbits.cpp
@@ -1,107 +1,109 @@
 #include "Networkbits.hpp"
 
+//Debugging output, use faster baud rate to remain responsive
 Serial pc(USBTX, USBRX, 115200);
 
-//Network thread - responsible for listening for connectinos and responding with updated tempature values
+//Network thread - This thread handles network communications over ethernet with a client
 void networktest()
-{   
-    //Configure an ethernet connection
+{
+    //Declare an ethernet interface
     EthernetInterface eth;
+
+    //Set properties
     eth.set_network(IP, NETMASK, GATEWAY);
+
+    //Connect to client
     eth.connect();
-    
-    //Now setup a web server
-    TCPSocket srv;           //TCP/IP Server
-    TCPSocket* clt_sock;      //Socket for communication
-    SocketAddress clt_addr;  //Address of incoming connection
-    
-    /* Open the server on ethernet stack */
+
+    //Setup a web server
+    //TCP/IP Server
+    TCPSocket srv;
+
+    //Socket for communication      
+    TCPSocket *clt_sock; 
+
+    //Address of incoming connection   
+    SocketAddress clt_addr; 
+
+    //Open the server on the ethernet interface stack
     srv.open(&eth);
-    
-    /* Bind the HTTP port (TCP 80) to the server */
+
+    //Bind the HTTP port (TCP 80) to the server
     srv.bind(eth.get_ip_address(), 80);
-    
-    /* Can handle 5 simultaneous connections */
-    srv.listen(5);
-    
-	int i = 0;
-    //KEEP RESPONDING WHILE THE SWITCHES ARE PRESSED
-	
-	//this will run once per http request
+
+    //Listen for 1 connection at a time
+    srv.listen(1);
+
+    //Start an infinite loop to handle http requests
+    //This will run once per http request recieved
     while (true) {
+        //Use the standard namespace to allow for easy use of the string type
         using namespace std;
 
-        char *buffer = new char[1024];
-        
+        //Create a buffer to hold the data sent from the client
+        //This will include the URL which will need to be parsed so that the appropiate action can be taken
+        char buffer[1024];
+
         //Block and wait on an incoming connection
         clt_sock = srv.accept();
-		
-        clt_sock->getpeername(&clt_addr);  //this will fill address of client to the SocketAddress object
 
+        //Fill address of client to the SocketAddress object
+        clt_sock->getpeername(&clt_addr); 
+
+        //Debugging, send the client information over serial
         pc.printf("accept %s:%d\n", clt_addr.get_ip_address(), clt_addr.get_port());
 
+        //Recieve the data sent from the client
         clt_sock->recv(buffer, 1024);
 
-        pc.printf("Received Msg: %s\n\n", buffer);  //this was missing in original example. 
-		
-		//address parser logic, decide what repsonse is required dependant on the incoming address
-		
-		string addFavicon ("favicon");
-		string address (buffer);
-		
-		
-		string response;
-		
-		
-		    //Read All from buffer
-		if (address.find(addFavicon) != string::npos) {
-			//Echo confirmation
-			pc.printf("favicon, 404");
-			
-			//Uses a C++ string to make it easier to concatinate
-			response += HTTP_STATUS_LINE_404;
-			
-			response += "\r\n";
-		
-		} 
-		else {
-			//Uses a C++ string to make it easier to concatinate
-			response += HTTP_STATUS_LINE_200;
-			
-			response += "\r\n";
-			
-			response += HTTP_HEADER_FIELDS;
-			
-			response += "\r\n\r\n";
-			
-			//Build the C++ string response
-			response += HTTP_MESSAGE_BODY1;
-		}
-		
-		
-        
-        
-		
-		//print send html
-		printf("\n\nHTML: %s", response.c_str());
-		
-		//Send static HTML response (as a C string)
-        clt_sock->send(response.c_str(), response.size());  
-		
-		
-		
-		//response = "HTTP/1.0 200 OK\r\n Content-Type: application/javascript; charset=utf-8\r\n";
-		
-		//Build the C++ string response
-        //response =+ jquery;
-		
-		//Send static HTML response (as a C string)
-        //clt_sock->send(response.c_str(), response.size()); 
+        //Debuging, print this out over serial
+        pc.printf("Received Msg: %s\n\n", buffer); //this was missing in original example.
 
-		clt_sock->close();
-		
-        delete[] buffer;
 
-		wait_us(100000);
+        //Address parser logic, decide what repsonse is required dependant on the incoming address
+
+        //Declare a string for the favicon url request - this needs to move to a more central location, header file
+        string addFavicon("favicon");
+
+        //Create a sting based on the recieved data from the client
+        string address(buffer);
+
+        //Create a string to hold the response to be sent to the client
+        string response;
+
+        //If the address contains the word favicon, return a 404 error
+        if (address.find(addFavicon) != string::npos) {
+            
+            //Add a 404 header to the response
+            response += HTTP_STATUS_LINE_404;
+
+            //Add a line feed and carriage return to the response
+            response += "\r\n";
+        }
+        else {
+            //Add a 200 header code to the response
+            response += HTTP_STATUS_LINE_200;
+
+            //Add a line feed and carriage return to the response
+            response += "\r\n";
+
+            //Add the header fields
+            response += HTTP_HEADER_FIELDS;
+
+            //Add 2 line feeds and carriage returns to the response to signal the end of the headers
+            response += "\r\n\r\n";
+
+            //Add the body
+            response += HTTP_MESSAGE_BODY1;
+        }
+
+        //Debugging, print the sent html
+        printf("\n\nHTML: %s", response.c_str());
+
+        //Send HTML response (as a C string)
+        clt_sock->send(response.c_str(), response.size());
+
+        //Close the socket to signal the end of the transmission
+        clt_sock->close();
     }
 }
