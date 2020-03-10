f3185d2936ae03095c858c2f65d53b38bf8c8dd4
Ross <ross@andrewarthacornwall.com>
2020-03-10 20:54:05 +0000

    Add a base device class

---
+++ devices/baseDevice.hpp
@@ -0,0 +1,47 @@
+#ifndef FIMAS_BASEDEVICE
+#define FIMAS_BASEDEVICE
+
+//Include mbed header file
+#include "mbed.h"
+
+/* 
+  * Base device class containng key method that all FIMAS devices use.
+  * This includes a unique ID as well as the control pins used
+  */
+class baseDevice
+{
+    //Private Properties
+private:
+    //Declare two digital outputs for the control pins. One or both may be used dependant on the device
+    DigitalOut *controlPin1, *controlPin2;
+
+    //Is the device using 1 or 2 control pins.
+    unsigned int noPins;
+
+    //The state of the device.
+    uint8_t state;
+
+    //The ID of the device, allowing the correct one to be controlled
+    unsigned short devID;
+
+    //Public API methods
+public:
+    //pin1 and pin2 specify the pin(s) that the device is connected to.
+    //deviceID uniquely identifies the device
+    baseDevice(PinName pin1, PinName pin2, unsigned short deviceID);
+
+    //pin specifies the pin that the device is connected to.
+    //deviceID uniquely identifies the device
+    baseDevice(PinName pin, unsigned short deviceID);
+
+    //Destructor
+    ~baseDevice();
+
+    //Get the ID of the device
+    unsigned short getID(void);
+
+    //Change the state of the device, in a interrupt-safe way
+    void changeState(int newState);
+};
+
+#endif
