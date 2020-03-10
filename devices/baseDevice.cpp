cb802808e84b92feafc3c4243da0cfc1ea129e05
Ross <ross@andrewarthacornwall.com>
2020-03-10 22:00:31 +0000

    Remove spaces

--- devices/baseDevice.cpp
+++ devices/baseDevice.cpp
@@ -4,37 +4,38 @@
 //pin specifies the pin that the device is connected to.
 //deviceID uniquely identifies the device
 baseDevice::baseDevice(PinName pin, unsigned short deviceID) {
-    
+    //Initialise the control pin used, default output state of low
+    this->controlPin1 = new DigitalOut(pin, 0);
+
+    //Set the device ID
+    this->devID = deviceID;
 }
-
-
 
 //Constructor
 //Pin specifies the pin that the pump is connected to
 perPump::perPump(PinName pin, unsigned short deviceID) {
-	this->controlPin = new DigitalOut(pin, state);
-	this->devID = deviceID;
+    this->controlPin = new DigitalOut(pin, state);
+    this->devID = deviceID;
 }
 
 //Destructor
 perPump::~perPump() {
-	delete controlPin;
+    delete controlPin;
 }
 
-//Change the state of the 
+//Change the state of the
 //0 turns the pump off, 1 turns the pump on
 void perPump::changeState(int newState) {
-	//Disable interrupts during critical section of a state change
-	CriticalSectionLock lock;
-	
-	//Update state variable
-	state = newState;
-	
-	//Update device control pin
-	controlPin->write(state);
+    //Disable interrupts during critical section of a state change
+    CriticalSectionLock lock;
+
+    //Update state variable
+    state = newState;
+
+    //Update device control pin
+    controlPin->write(state);
 }
 
-unsigned short perPump::getID (void) {
-	return devID;
+unsigned short perPump::getID(void) {
+    return devID;
 }
-
