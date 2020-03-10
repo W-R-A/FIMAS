30c7ae93382cdc108f6f7fd41464f77c0eaf7942
Ross <ross@andrewarthacornwall.com>
2020-03-10 21:55:20 +0000

    Update Formatting

--- devices/baseDevice.hpp
+++ devices/baseDevice.hpp
@@ -1,46 +1,46 @@
 #ifndef FIMAS_BASEDEVICE
 #define FIMAS_BASEDEVICE
 
-//Include mbed header file
+// Include mbed header file
 #include "mbed.h"
 
-/* 
-  * Base device class containng key methods that all FIMAS devices use.
-  * This includes a unique ID as well as the control pins used
-  */
-class baseDevice
-{
-    //Private Properties
+/*
+ * Base device class containng key methods that all FIMAS devices use.
+ * This includes a unique ID as well as the control pins used
+ */
+class baseDevice {
+    // Private Properties
 private:
-    //Declare two digital outputs for the control pins. One or both may be used dependant on the device
+    // Declare two digital outputs for the control pins. One or both may be used
+    // dependant on the device
     DigitalOut *controlPin1, *controlPin2;
 
-    //Is the device using 1 or 2 control pins.
+    // Is the device using 1 or 2 control pins.
     unsigned int noPins;
 
-    //The state of the device.
+    // The state of the device.
     uint8_t state;
 
-    //The ID of the device, allowing the correct one to be controlled
+    // The ID of the device, allowing the correct one to be controlled
     unsigned short devID;
 
-    //Public API methods
+    // Public API methods
 public:
-    //pin1 and pin2 specify the pin(s) that the device is connected to.
-    //deviceID uniquely identifies the device
+    // pin1 and pin2 specify the pin(s) that the device is connected to.
+    // deviceID uniquely identifies the device
     baseDevice(PinName pin1, PinName pin2, unsigned short deviceID);
 
-    //pin specifies the pin that the device is connected to.
-    //deviceID uniquely identifies the device
+    // pin specifies the pin that the device is connected to.
+    // deviceID uniquely identifies the device
     baseDevice(PinName pin, unsigned short deviceID);
 
-    //Destructor
+    // Destructor
     ~baseDevice();
 
-    //Get the ID of the device
+    // Get the ID of the device
     unsigned short getID(void);
 
-    //Change the state of the device, in a interrupt-safe way
+    // Change the state of the device, in a interrupt-safe way
     void changeState(int newState);
 };
 
