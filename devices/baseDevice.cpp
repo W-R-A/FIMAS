e0d8044aaa055d2ed6cf51b9f04f706721e22cfb
Ross <ross@andrewarthacornwall.com>
2020-03-10 22:09:21 +0000

    Update base device methods

--- devices/baseDevice.cpp
+++ devices/baseDevice.cpp
@@ -9,33 +9,55 @@ baseDevice::baseDevice(PinName pin, unsigned short deviceID) {
 
     //Set the device ID
     this->devID = deviceID;
+
+    //Set that this is a one pin device
+    this->noPins = 1;
+
+    //Initialise the state to be zero
+    this->state = 0;
 }
 
 //Constructor
-//Pin specifies the pin that the pump is connected to
-perPump::perPump(PinName pin, unsigned short deviceID) {
-    this->controlPin = new DigitalOut(pin, state);
+//pin1 and pin2 specify the pin(s) that the device is connected to.
+//deviceID uniquely identifies the device
+baseDevice::baseDevice(PinName pin1, PinName pin2, unsigned short deviceID) {
+    //Initialise the control pin used, default output state of low
+    this->controlPin1 = new DigitalOut(pin1, 0);
+
+    //Initialise the second control pin used, default output state of low
+    this->controlPin2 = new DigitalOut(pin2, 0);
+
+    //Set the device ID
     this->devID = deviceID;
+
+    //Set that this is a two pin device
+    this->noPins = 2;
+
+    //Initialise the state to be zero
+    this->state = 0;
 }
 
 //Destructor
-perPump::~perPump() {
-    delete controlPin;
+baseDevice::~baseDevice() {
+    //Delete control pin one
+    delete controlPin1;
+
+    //If two pins used, delete the second control pin
+    if (this->noPins == 2) {
+        delete controlPin2;
+    }
 }
 
-//Change the state of the
-//0 turns the pump off, 1 turns the pump on
-void perPump::changeState(int newState) {
+//Change the state of the internal state variable in an interrupt-safe way
+void baseDevice::changeState(int newState) {
     //Disable interrupts during critical section of a state change
     CriticalSectionLock lock;
 
     //Update state variable
     state = newState;
-
-    //Update device control pin
-    controlPin->write(state);
 }
 
-unsigned short perPump::getID(void) {
+//Get the ID of the device
+unsigned short baseDevice::getID(void) {
     return devID;
 }
