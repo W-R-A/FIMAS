1be8c3d5c50fdc609c77d4306c95ff4731b68316
Ross <ross@andrewarthacornwall.com>
2020-03-12 14:49:52 +0000

    Create a switching valve class

---
+++ devices/switchValve.cpp
@@ -0,0 +1,104 @@
+#include "switchValve.hpp"
+
+////pin specifies the pin that the switching valve step input is connected to
+//deviceID uniquely identifies the device
+//Constructor is inhertited from baseDevice
+switchingValve::switchingValve(PinName pin, unsigned short deviceID) : baseDevice(pin, deviceID) {}
+
+//pin1 specifies the pin that the switching valve step input is connected to
+//pin2 specifies the pin that the switching valve home input is connected to
+//deviceID uniquely identifies the device
+//Constructor is inhertited from baseDevice
+switchingValve::switchingValve(PinName pin1, PinName pin2, unsigned short deviceID) : baseDevice(pin1, pin2, deviceID) {}
+
+//create the signal pulses needed to change the state of the switching valve
+//pin determines the pin to generate the pulses on
+//noPulses determines the number of pulses generated
+//According to the data sheet, each pulse must be at least 30ms in duration, so a delay of 50ms is used
+void switchingValve::pulse(unsigned short pin, unsigned short noPulses)
+{
+    for (int i = 0; i < noPulses; i++) {
+        if (pin == 1) {
+            this->controlPin1->write(1);
+            thread_sleep_for(50);
+            this->controlPin1->write(0);
+        }
+        else {
+            this->controlPin2->write(1);
+            thread_sleep_for(50);
+            this->controlPin2->write(0);
+        }
+    }
+}
+
+//Change the state of the switching valve
+//The valve can be in one of ten states and can operated in two modes
+//Hence newstate is the desired state of the valve
+unsigned short switchingValve::changeState(unsigned short newState)
+{
+    //Check if operating in 1 or 2 pin mode
+    //If in single pin mode - need to cycle through all configurations to get to the derired state
+    if (this->noPins == 1) {
+        //Bounds Check
+        if (newState > 10) {
+            printError("State out of bounds");
+        }
+        else {
+            //Define how many pulses are needed to move to the next state
+            unsigned short pulses = 0;
+            //If the current state of greater than th desired state, get back to position zero before moving to the desired state
+            if (this->state > newState) {
+                pulses = 10 - this->state;
+                pulses = pulses + newState;
+            }
+            else {
+                pulses = newState;
+            }
+
+            //Change to desired state
+            pulse(1, pulses);
+
+            updateState(newState);
+        }
+    }
+    //If in 2 pin mode, reset to home before cycling through to the desired state
+    else {
+        //Bounds Check
+        if (newState > 10) {
+            printError("State out of bounds");
+        }
+        else {
+            //Reset position
+            pulse(2, newState);
+
+            //Change to desired state
+            pulse(1, newState);
+
+            updateState(newState);
+        }
+    }
+}
+
+//If the newstate is non-zero, and the pump is not already running, turn the valve on
+if (newState) {
+    if (this->state == 1) {
+        return this->state;
+    }
+    else {
+        this->controlPin1->write(1);
+        updateState(1);
+        return this->state;
+    }
+}
+//If the newstate is zero, and the pump is running, turn the valve off
+else {
+    if (this->state == 0) {
+        return this->state;
+    }
+    else {
+        this->controlPin1->write(0);
+        updateState(0);
+        return this->state;
+    }
+}
+}
---
+++ devices/switchValve.hpp
@@ -0,0 +1,39 @@
+#ifndef FIMAS_SWITCHVALVE
+#define FIMAS_SWITCHVALVE
+
+//Include mbed header file
+#include "mbed.h"
+
+//Include the base device header file
+#include "baseDevice.hpp"
+
+/*
+ * Switching valve class containing key methods to control a switching valve.
+ * Based off of the baseDevice class
+ */
+
+class switchingValve : public baseDevice {
+    //Public API
+public:
+    //pin specifies the pin that the switching valve step input is connected to
+    //deviceID uniquely identifies the device
+    switchingValve(PinName pin, unsigned short deviceID);
+
+    //pin1 specifies the pin that the switching valve step input is connected to
+    //pin2 specifies the pin that the switching valve home input is connected to
+    //deviceID uniquely identifies the device
+    switchingValve(PinName pin1, PinName pin2, unsigned short deviceID);
+
+    //Change the state of the switching valve
+    //The valve can be in one of ten states and can operated in two modes
+    unsigned short changeState(unsigned short newState);
+
+    //Protected
+protected:
+    //create the signal pulses needed to change the state of the switching valve
+    //pin determines the pin to generate the pulses on
+    //noPulses determines the number of pulses generated
+    void pulse(unsigned short pin, unsigned short noPulses);
+};
+
+#endif
