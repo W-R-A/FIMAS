bc4ba9bf14165bc30c64fb7f70b89b0c0cb45562
Ross <ross@andrewarthacornwall.com>
2020-04-11 23:25:00 +0100

    Add device configuration parsing functions

---
+++ configuration/deviceConfigParser.hpp
@@ -0,0 +1,11 @@
+#ifndef FIMAS_DEVICECONFIGPARSER
+#define FIMAS_DEVICECONFIGPARSER
+
+//Include mbed header file
+#include "mbed.h"
+
+//Include JSON Parser header file
+#include "MbedJSONValue.h"
+
+
+#endif
