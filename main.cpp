6c973ce9778fd0af1ad50f9d67921dd29de4cd42
Ross <ross@andrewarthacornwall.com>
2020-04-13 13:01:23 +0100

    reorder includes

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@
  * Ross Andrewartha, January 2020
  */
 
-#include "Networkbits.hpp"
-#include "deviceConfig.hpp"
-#include "hardware.hpp"
 #include "serialInterface.hpp"
+#include "hardware.hpp"
+#include "deviceConfig.hpp"
+#include "Networkbits.hpp"
+
 #include <array>
 #include <string>
 #include <vector>
