eca8446b923752cf7bb08db168dad1bf7c813298
Ross <ross@andrewarthacornwall.com>
2020-09-17 22:22:34 +0100

    add routes for pages

--- html/app/routes.py
+++ html/app/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, url_for
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Main Page')
+    return render_template('index.html', title='Home')
+
+
+@app.route('/devices')
+def devices():
+    return render_template('index.html', title='Device Configuration')
+
+
+@app.route('/routines')
+def routines():
+    return render_template('index.html', title='Routines Configuration')
+
+
+@app.route('/estop')
+def estop():
+    return render_template('index.html', title='Emergency Stop')
