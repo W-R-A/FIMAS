cbc09b2d916c8eb0da48c2725675e79b5b9576a4
Ross <ross@andrewarthacornwall.com>
2020-09-18 20:47:35 +0100

    declare device types

--- html/app/data.py
+++ html/app/data.py
@@ -9,3 +9,31 @@ def getDevices():
     devices = '[{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"swichValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]'
 
     return devices
+
+
+@app.route('/gettypes')
+def getTypes():
+    # ToDo - Database lookup of current device types
+
+    deviceTypes = '''
+    [
+    {
+        "devType": "perPump",
+        "friendlyName": "Peristaltic Pump"
+    },
+    {
+        "devType": "solValve",
+        "friendlyName": "Solenoid Valve"
+    },
+    {
+        "devType": "sixValve",
+        "friendlyName": "6-Port Valve"
+    },
+    {
+        "devType": "switchValve",
+        "friendlyName": "Switching Valve"
+    }
+    ]
+    '''
+
+    return deviceTypes
