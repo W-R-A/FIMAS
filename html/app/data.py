d296b1f171d163ac20b27e226e1d20845d85bba6
Ross <ross@andrewarthacornwall.com>
2020-09-18 21:39:52 +0100

    Fix typo

--- html/app/data.py
+++ html/app/data.py
@@ -6,7 +6,7 @@ from app import app
 def getDevices():
     # ToDo - Database lookup of current devices
 
-    devices = '[{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"swichValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]'
+    devices = '[{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"switchValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]'
 
     return devices
 
