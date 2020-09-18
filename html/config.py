8fee3c2546c90c4171d351af00fb5ae78687035f
Ross <ross@andrewarthacornwall.com>
2020-09-18 23:21:04 +0100

    More config data

--- html/config.py
+++ html/config.py
@@ -6,3 +6,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'random secret key'
     BOOTSTRAP_SERVE_LOCAL = os.environ.get('BOOTSTRAP_SERVE_LOCAL') or True
+    DEVICETYPES = [{"devType": "perPump", "friendlyName": "Peristaltic Pump"}, {"devType": "solValve", "friendlyName": "Solenoid Valve"}, {"devType": "sixValve", "friendlyName": "6-Port Valve"}, {"devType": "switchValve", "friendlyName": "Switching Valve"}]
