5b87123bd3c44ded44d8f0de7a7a313d434400d9
Ross <ross@andrewarthacornwall.com>
2020-09-18 23:19:35 +0100

    Convert to python and only jsonify when needed

--- html/app/data.py
+++ html/app/data.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
 
 
@@ -6,34 +6,15 @@ from app import app
 def getDevices():
     # ToDo - Database lookup of current devices
 
-    devices = '[{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"switchValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]'
+    devices = [{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"switchValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]
 
-    return devices
+    return jsonify(devices)
 
 
 @app.route('/gettypes')
 def getTypes():
     # ToDo - Database lookup of current device types
 
-    deviceTypes = '''
-    [
-    {
-        "devType": "perPump",
-        "friendlyName": "Peristaltic Pump"
-    },
-    {
-        "devType": "solValve",
-        "friendlyName": "Solenoid Valve"
-    },
-    {
-        "devType": "sixValve",
-        "friendlyName": "6-Port Valve"
-    },
-    {
-        "devType": "switchValve",
-        "friendlyName": "Switching Valve"
-    }
-    ]
-    '''
+    #deviceTypes = [{"devType": "perPump", "friendlyName": "Peristaltic Pump"}, {"devType": "solValve", "friendlyName": "Solenoid Valve"}, {"devType": "sixValve", "friendlyName": "6-Port Valve"}, {"devType": "switchValve", "friendlyName": "Switching Valve"}]
 
-    return deviceTypes
+    return jsonify(app.DEVICETYPES)
