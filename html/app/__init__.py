eb2c8cc029af16472fe5eccf52fb374ecdbdfce4
Ross <ross@andrewarthacornwall.com>
2020-09-19 20:22:24 +0100

    Add database

--- html/app/__init__.py
+++ html/app/__init__.py
@@ -1,10 +1,30 @@
+#Imports
+#Flask
 from flask import Flask
+
+#Configuration
 from config import Config
+
+#Database and migration
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#Bootstrap
 from flask_bootstrap import Bootstrap
 
+
+#Create app
 app = Flask(__name__)
+
+#Configure it
 app.config.from_object(Config)
 
+#Setup database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#Import routes 
 from app import routes, errors, data
 
+#Start bootstrap
 bootstrap = Bootstrap(app)
