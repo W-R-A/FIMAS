fcbf8ecce58dfef49e56fd5dffdb38a43170c792
Ross <ross@andrewarthacornwall.com>
2020-09-19 22:35:37 +0100

    allow for shell running

--- html/fimas.py
+++ html/fimas.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import Routine, Device, Timing
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Routine': Routine, 'Device': Device, 'Timing': Timing}
