06d9a6eeec48a96ee2cd086001baf08cc816cf6e
Ross <ross@andrewarthacornwall.com>
2020-09-19 18:04:01 +0100

    Add a form to delete a device

--- html/app/forms.py
+++ html/app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-class ADDDEVICEFORM(FlaskForm):
+class AddDeviceForm(FlaskForm):
     devTypes = [{"devType": "perPump", "friendlyName": "Peristaltic Pump"}, {"devType": "solValve", "friendlyName": "Solenoid Valve"}, {"devType": "sixValve", "friendlyName": "6-Port Valve"}, {"devType": "switchValve", "friendlyName": "Switching Valve"}]
     devPins = [{"pin":"1","type":"output"},{"pin":"2","type":"output"},{"pin":"3","type":"output"},{"pin":"4","type":"output"},{"pin":"5","type":"output"},{"pin":"6","type":"output"},{"pin":"7","type":"output"},{"pin":"8","type":"output"},{"pin":"1","type":"input"},{"pin":"2","type":"input"},{"pin":"3","type":"input"},{"pin":"4","type":"input"}]
 
@@ -29,3 +29,20 @@ class ADDDEVICEFORM(FlaskForm):
 
     devPin2 = SelectField('Secondary Interface', choices = devPinChoices2, validators=[DataRequired()])
     submit = SubmitField('Add Device')
+
+
+
+class DeleteDeviceForm(FlaskForm):
+
+    devices = [{"devID":"1000","devName":"Sample Pump","devType":"perPump","devPin1":"2","devPin2":"3"},{"devID":"1001","devName":"Distribution Valve","devType":"switchValve","devPin1":"5","devPin2":"6"},{"devID":"1002","devName":"6-Port Valve","devType":"sixValve","devPin1":"9","devPin2":"10"}]
+
+    deviceChoices = []
+
+    for j in range (0, len(devices)):
+        deviceChoices.append([devices[j]["devID"], devices[j]["devName"]])
+
+
+    devID = SelectField('Device Name', choices = deviceChoices, validators=[DataRequired()])
+    submit = SubmitField('Delete Device')
+
+
