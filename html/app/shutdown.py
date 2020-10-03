1f8a119ea3161d6330d9a19a6759c6396ca0bbef
Ross <ross@andrewarthacornwall.com>
2020-10-03 22:57:38 +0100

    shutdown working

--- html/app/shutdown.py
+++ html/app/shutdown.py
@@ -1,9 +1,15 @@
 from flask import url_for, redirect
 from app import app
 
-import os
+import subprocess
+import shlex
+
+
+
 
 @app.route('/shutdown')
 def shutdown():
-    os.system('systemctl poweroff')
+    cmd = shlex.split("sudo shutdown -h now")
+    subprocess.call(cmd)
+    flash("Shutdown request sucessful, please wait for 30 seconds before unplugging the USB cable!")
     return redirect(url_for('index'))
