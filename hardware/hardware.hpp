c40d99638477d985656a25a34313c6bda18d5356
Ross <ross@andrewarthacornwall.com>
2020-10-04 20:48:51 +0100

    remove redundant code

--- hardware/hardware.hpp
+++ hardware/hardware.hpp
@@ -7,9 +7,6 @@
 //Include array header
 #include <array>
 
-//Include vector header
-#include <vector>
-
 //Include the base device class
 #include "baseDevice.hpp"
 
@@ -34,51 +31,5 @@ extern const array<PinName, 4> digitalInputs;
 
 //Calculate the number of digital inputs and outpus defined
 const short maxDevices = digitalOutputs.size() + digitalInputs.size();
-
-//Declare an array to hold the devices given that each device requires an minimum of one pin, this means that the number of devices is limited by the number of digital output and input pins defined
-//Contents will be populated at run-time
-//extern vector<baseDevice *> devices;
-
-// //Define enumerated types for the devices used
-// enum DEVICE {WASHPUMP, SAMPLEPUMP, SWITCHVALVE, SOLENOIDVALVE};
-
-// //Declare how many devices there are defined above
-// #define NUMBER_DEVICES 4
-
-// //Declare DigitalOutputs for the utilised devices
-// extern DigitalOut washPump;
-// extern DigitalOut samplePump;
-// extern DigitalOut switchValve;
-// extern DigitalOut solenoidValve;
-
-//Declare input userbutton as trigger
-//extern DigitalIn userButton;
-
-// //Serial Communications
-// extern Serial pc;
-
-
-
-// //Define a structure to hold the timing parameters for all the devices in a step
-// typedef struct {
-//     deviceTime StepTimes[NUMBER_DEVICES];
-// } step;
-
-//Function prototypes
-
-//Perform a Power On Self Test, POST
-/**
-    @param iter 
-*/
-// void post (uint8_t iter);
-
-// //Device timings - This is called once a second and turns devices on or off based on the timing parameters
-// void checkTiming (void);
-
-// //Turn on a device
-// void turnOn (enum DEVICE device);
-
-// //Turn off a device
-// void turnOff (enum DEVICE device);
 
 #endif
